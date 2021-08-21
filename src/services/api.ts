92ab7fde6458428f8214707e1dcd3971fede8493
Paulo Thor <paulothor12345@gmail.com>
2021-08-20 21:25:28 -0300

    fix api url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://my-json-server.typicode.com/EnkiGroup/DesafioReactEncontact2021",
+  baseURL:
+    "https://my-json-server.typicode.com/EnkiGroup/DesafioReactEncontact2021",
 });
 
 export default api;
